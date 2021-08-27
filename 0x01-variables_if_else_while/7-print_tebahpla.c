8f771dee8f48111b999e7dbe823471fed1ba4a15
Aubin13 <dashunix@DASH.localdomain>
2021-08-27 17:20:22 +0200

    8

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - s
+ *Return: 0
+ */
+int main(void)
+{
+char ahpla;
+for (ahpla = 'z'; ahpla >= 'a'; ahpla--)
+{
+putchar(ahpla);
+}
+putchar('\n');
+return (0);
+}
