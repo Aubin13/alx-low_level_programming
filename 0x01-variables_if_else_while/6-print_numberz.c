b735eff56ce67b0b9f72f619fbbc584793a8845f
Aubin13 <dashunix@DASH.localdomain>
2021-08-27 17:10:57 +0200

    7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - execute
+ *Return: 0
+ */
+int main(void)
+{
+int a
+for (a = 0; a < 10; a++)
+{
+putchar(48 + c);
+}
+putchar('\n');
+return (0);
+}
