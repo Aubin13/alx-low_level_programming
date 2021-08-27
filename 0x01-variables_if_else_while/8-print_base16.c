29a70a49a4199f31a222344d818f662ffee3c967
Aubin13 <dashunix@DASH.localdomain>
2021-08-27 17:25:15 +0200

    9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - d
+ *Return: 0
+ */
+int main(void)
+{
+int a;
+char b;
+for (a = 0; a < 10; a++)
+{
+printf("%d", a);
+}
+for(b = 'a'; b <= 'f'; b++)
+{
+putchar(b);
+}
+putchar('\n');
+}
