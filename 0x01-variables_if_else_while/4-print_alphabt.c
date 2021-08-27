16a6621765672e7acd0127af486e039bb77a9741
Aubin13 <dashunix@DASH.localdomain>
2021-08-27 16:45:00 +0200

    5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - execution
+ *Return: o
+ */
+int main (void)
+{
+char alpha;
+for (alpha='a'; alpha<= 'z'; alpha++)
+{
+if (alpha != 'q') && (alpha != 'e')
+putchar('c');
+}
+putchar('\n);
+return (0);
+}
