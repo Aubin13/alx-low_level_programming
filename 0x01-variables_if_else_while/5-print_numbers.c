10d55a4340a20507aa0af3143a96d5b76af0a9b6
Aubin13 <dashunix@DASH.localdomain>
2021-08-27 16:58:10 +0200

    6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - execution
+ *Return: 0
+ */
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+printf(a);
+}
+printf("\n");
+return (0);
+}
