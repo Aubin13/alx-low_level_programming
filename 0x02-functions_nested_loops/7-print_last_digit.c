f87ff4cce5f6cbb7af8d59d0de54582a96b6e089
Aubin13 <dashunix@DASH.localdomain>
2021-08-31 17:19:43 +0200

    ss

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,17 @@
 #include "main.h"
 /**
- * print_last_digit - execution
- * @x: integer
- * Return: something
+ * print_last_digit - return last digit
+ * @n : number to check
+ * Return:0 or 1
  */
-int print_last_digit(int x)
+
+int print_last_digit(int n)
 {
-return (x % 10);
+int nv;
+if (n < 0)
+nv = -1 * (n % 10);
+else
+nv = n % 10;
+_putchar(nv + '0');
+return (nv);
 }
