a5dcdb5c45ccad40efd5d05b55219db0783a2028
Aubin13 <dashunix@DASH.localdomain>
2021-08-31 17:52:51 +0200

    10

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,17 +1,32 @@
 #include "main.h"
 /**
- * times_table - execution
- * Return: 0
+ * times_table - prints the times table
  */
 void times_table(void)
 {
-int x, y;
-for (x = 0; x < 10; x++)
+int i, j, product;
+for (i = 0; i < 10; i++)
 {
-for (y = 0; y <= 9; y++)
+for (j = 0; j < 10; j++)
+}
+if (j == 0)
 {
-_putchar(x * y + 48);
+_putchar(48);
+continue;
+}
+product = i * j;
 _putchar(',');
+_putchar(' ');
+if (product >= 10)
+{
+_putchar(product / 10 + 48);
+_putchar(product % 10 + 48);
+}
+else
+{
+_putchar(' ');
+_putchar(product + 48);
+}
 }
 _putchar('\n');
 }
