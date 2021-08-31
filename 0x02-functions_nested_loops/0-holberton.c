5148f37eea28d7f20ef5edddd09b6150e4c0a98f
Aubin13 <dashunix@DASH.localdomain>
2021-08-31 16:06:32 +0200

    x

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - print Holberton
  * Return : 0
  */
-int main(void){
-putchar("Holberton");
-putchar('\n');
+int main(void)
+{
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 return (0);
 }
