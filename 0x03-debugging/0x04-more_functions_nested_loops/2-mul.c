f3fc97a3d9995f24e8fa5f8a91ad1a9a7dc602c3
Aubin13 <dashunix@DASH.localdomain>
2021-09-02 18:17:02 +0200

    New project

---
+++ 0x03-debugging/0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - execution
+ * @a: integer one
+ * @b: integer two
+ * Return: product
+ */
+int mul(int a, int b)
+{
+int product;
+product = a * b;
+return (product);
+}
---
+++ 0x03-debugging/0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - execution
+ * @size: size of square
+ * Return: size
+ */
+void print_square(int size)
+{
+int y, x;
+if (size > 0)
+{
+for (x = 0; x <= size; x++)
+{
+for (y = 0; y <= size; y++)
+{
+_putchar('\#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
