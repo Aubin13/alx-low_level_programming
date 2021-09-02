a885c68d235c5490c066335e01b39906c2170ced
Aubin13 <dashunix@DASH.localdomain>
2021-09-02 18:53:37 +0200

    fix 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,25 +1,28 @@
 #include "main.h"
+
 /**
- * print_square - execution
- * @size: size of square
- * Return: 0
+ * print_square - a function that prints a square, followed by a new line
+ * @size: size of both width and length
+ * Return: a square made of '#'
  */
 void print_square(int size)
 {
-int y, x;
-if (size > 0)
+int co, ro;
+
+if (size <= 0)
 {
-for (x = 0; x <= size; x++)
+_putchar('\n');
+}
+else
 {
-for (y = 0; y <= size; y++)
+for (co = 1; co <= size; co++)
+{
+_putchar('#');
+for (ro = 2; ro <= size; ro++)
 {
 _putchar('#');
 }
 _putchar('\n');
 }
-}
- else if (size <= 0)
-{
-_putchar('\n');
 }
 }
