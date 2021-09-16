1c1231e54ac717462de3268d4abffbc4a45209ec
Aubin13 <84918479+Aubin13@users.noreply.github.com>
2021-09-16 14:48:49 +0200

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,22 @@
-#include "main.h"
+/*
+ * File: 0-puts_recursion.c
+ * Auth: Ukonu Divine Chisom
+ */
+
+#include "holberton.h"
 
 /**
- * _puts_recursion - print string with new line
- * @s: string to print
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
  */
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
-{
-_putchar('\n');
-return;
-}
-_putchar(*s);
-s++;
-_puts_recursion(s);
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
 }
