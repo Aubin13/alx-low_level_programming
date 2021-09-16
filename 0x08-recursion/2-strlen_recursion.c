288f932bfdae97d040b61be6180698efdce4ceb5
Aubin13 <84918479+Aubin13@users.noreply.github.com>
2021-09-16 15:30:07 +0200

    okay okay 🇷🇼🇷🇼

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,18 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
-*_strlen_recursion - execution
-*@s: variable in question
-*@counter: the variable to count the length
-*Return: integer
-*/
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
 int _strlen_recursion(char *s)
 {
-  int counter = 0;
+int len = 0;
 if (*s)
 {
-counter++;
-counter += _strlen_recursion(s + 1);
+len++;
+len += _strlen_recursion(s + 1);
 }
-return (counter);
+return (len);
 }
