70f22222d1f08644d0ef1a2566d59f69ca0c5e2b
Aubin13 <84918479+Aubin13@users.noreply.github.com>
2021-09-16 15:15:08 +0200

    Create 2-strlen-recursion.c
    number 2 🇷🇼

---
+++ 0x08-recursion/2-strlen-recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*_strlen_recursion - execution
+*@s: variable in question
+*@counter: the variable to count the length
+*Return: integer
+*/
+int _strlen_recursion(char s)
+{
+int counter = 0;
+if (s)
+{
+counter ++;
+}
+else
+{
+return;
+}
+return (counter);
+}
