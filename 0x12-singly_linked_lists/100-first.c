c79d6856e5b05a8acff1973a52e76db8a8ccc1ed
Aubin13 <84918479+Aubin13@users.noreply.github.com>
2021-10-07 18:28:46 +0200

    update 👌🏿🦸‍♂️

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-void theHare(void)__attribute__((constructor));
+void __attribute__((constructor)) hare(void);
 
 /**
- * theHare - prints You're beat! and yet, you must allow,\n
- * I bore my house upon my back!\n before the main function is executed.
+ * hare - Prints a string before the
+ *        main function is executed.
  */
-void theHare(void)
+void hare(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
