7c35872ce4222ceccf25c57f35c8e784cc711a64
Aubin Dash <84918479+Aubin13@users.noreply.github.com>
2023-10-24 14:28:58 +0200

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - finds the loop in a linked list
+ * find_listint_loop - finds loop in a linked list
  * @head: linked list to search for
  *
- * Return: address of the node where the loop starts, or NULL
+ * Return: address of the node where the loop begins, or NULL
  */
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow = head;
 	listint_t *fast = head;
 
-	if (head == NULL)
+	if (!head)
 		return (NULL);
 
-	while (slow && fast && (fast->next))
+	while (slow && fast && fast->next)
 	{
 		fast = fast->next->next;
 		slow = slow->next;
@@ -26,10 +26,8 @@ listint_t *find_listint_loop(listint_t *head)
 				slow = slow->next;
 				fast = fast->next;
 			}
-
 			return (fast);
 		}
 	}
-
 	return (NULL);
 }
