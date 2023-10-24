e49749cdfdb6a56330d59c7f943954cdbfa25da9
Aubin Dash <kiaubin13@gmail.com>
2023-10-24 14:18:17 +0200

    yah

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,8 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SUCCESS 1
-#define FAILURE -1
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -20,7 +18,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -37,4 +34,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif /* LISTS_H */
+#endif
